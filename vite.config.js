cad681992d351292a8e376c53410872564589269
Raphael Gomes <raphael.ragss@gmail.com>
2026-07-12 13:19:47 -0300

    Refactor vite.config.js for server and build settings

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react()],
 
   server: {
-    port: 5173,
     host: true,
+    port: 5173,
+  },
+
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
 
   build: {
     outDir: 'dist',
     sourcemap: true,
-    rollupOptions: {
-      input: './index.html',   // Mudança importante aqui
-    }
   },
-
-  resolve: {
-    alias: {
-      '/': '/src'
-    }
-  },
-
-  base: './'
 })
