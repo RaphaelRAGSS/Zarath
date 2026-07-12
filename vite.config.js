341cc7c75347ba575fb4971f2ee69f7658fd54ba
Raphael Gomes <raphael.ragss@gmail.com>
2026-07-12 13:27:13 -0300

    Remove alias and build configurations from Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react()],
@@ -8,16 +7,5 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-  },
-
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
   },
 })
