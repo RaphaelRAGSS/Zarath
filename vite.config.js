80c1ef58070f05e0215ea237a6859811c3251266
Raphael Gomes <raphael.ragss@gmail.com>
2026-07-12 12:36:24 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,30 +4,24 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
-  // Configurações de desenvolvimento
   server: {
     port: 5173,
-    host: true, // expõe na rede local
+    host: true,
   },
 
-  // Configurações de Build (essencial para Vercel)
   build: {
     outDir: 'dist',
     sourcemap: true,
     rollupOptions: {
-      input: {
-        main: './index.html'
-      }
+      input: './index.html',   // Mudança importante aqui
     }
   },
 
-  // Resolve o erro de importação /src/main.jsx
   resolve: {
     alias: {
       '/': '/src'
     }
   },
 
-  // Base para Vercel
   base: './'
 })
